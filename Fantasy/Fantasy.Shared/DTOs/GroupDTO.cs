c904014424825a1f77c761f27e68fc9389534ad1
Edward Javier Pineda Moran <edwardjpineda@outlook.com>
2024-12-10 20:44:22 -0600

    Add Groups controller

---
+++ Fantasy/Fantasy.Shared/DTOs/GroupDTO.cs
@@ -0,0 +1,34 @@
+﻿using Fantasy.Shared.Resources;
+using System.ComponentModel.DataAnnotations;
+
+namespace Fantasy.Shared.DTOs;
+
+public class GroupDTO
+{
+    public int Id { get; set; }
+
+    [Display(Name = "Group", ResourceType = typeof(Literals))]
+    [MaxLength(100, ErrorMessageResourceName = "MaxLength", ErrorMessageResourceType = typeof(Literals))]
+    [Required(ErrorMessageResourceName = "RequiredField", ErrorMessageResourceType = typeof(Literals))]
+    public string Name { get; set; } = null!;
+
+    [Display(Name = "Admin", ResourceType = typeof(Literals))]
+    [MaxLength(450, ErrorMessageResourceName = "MaxLength", ErrorMessageResourceType = typeof(Literals))]
+    public string AdminId { get; set; } = null!;
+
+    [Display(Name = "Tournament", ResourceType = typeof(Literals))]
+    [Range(1, int.MaxValue, ErrorMessageResourceName = "RequiredField", ErrorMessageResourceType = typeof(Literals))]
+    public int TournamentId { get; set; }
+
+    [Display(Name = "Code", ResourceType = typeof(Literals))]
+    [MaxLength(6, ErrorMessageResourceName = "MaxLength", ErrorMessageResourceType = typeof(Literals))]
+    public string Code { get; set; } = null!;
+
+    public string? Image { get; set; }
+
+    [Display(Name = "IsActive", ResourceType = typeof(Literals))]
+    public bool IsActive { get; set; }
+
+    [Display(Name = "Remarks", ResourceType = typeof(Literals))]
+    public string? Remarks { get; set; }
+}
