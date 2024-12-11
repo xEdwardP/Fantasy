716f89b5df1dc4945da7609aa90b08c00a770d8b
Edward Javier Pineda Moran <edwardjpineda@outlook.com>
2024-12-11 13:52:18 -0600

    Add PredictionsController and Create and Edit Groups

---
+++ Fantasy/Fantasy.Frontend/Helpers/ClipboardService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.JSInterop;
+
+namespace Fantasy.Frontend.Helpers;
+
+public class ClipboardService : IClipboardService
+{
+    private readonly IJSRuntime _jsRuntime;
+
+    public ClipboardService(IJSRuntime jsRuntime)
+    {
+        _jsRuntime = jsRuntime;
+    }
+
+    public async Task CopyToClipboardAsync(string text)
+    {
+        await _jsRuntime.InvokeVoidAsync("copyToClipboard", text);
+    }
+}
